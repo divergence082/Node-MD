9c43467a4086f1458cc2f171717eca966126eab1
Valeria <divergence082@gmail.com>
2014-08-24 21:22:35 +0400

    parsers

--- test/client.js
+++ test/client.js
@@ -1,7 +1,7 @@
 
 var md = require('../bin/index.js');
 
-var client = new md.Client(6379, '127.0.0.1');
+var client = new md.redis.Client(6379, '127.0.0.1');
 var handler = new md.PacketHandler(function(cursor, chunk) {return true}, function() {console.log('RESULT')});
 
 client._request('KEYS *', handler);
